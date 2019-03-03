e4a45071a7872520f5d7937cd4ffe1087396ab1e
rudydekeerschieter <44776187+rudydekeerschieter@users.noreply.github.com>
2019-03-03 14:46:25 +0100

    First commit

---
+++ src/main/java/be/ucll/feedback/Application.java
@@ -0,0 +1,11 @@
+package be.ucll.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // need to tell Spring this is a Spring Boot Application
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
